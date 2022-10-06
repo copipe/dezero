b622a834df0e64dd6f8513817fd77e55589d9975
copipe <taka.0122.kuro@gmail.com>
2022-10-06 04:33:06 +0000

    add test_step07

---
+++ tests/steps/test_step07.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from steps.step07 import Exp, Square, Variable
+
+
+def test_chain():
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    assert y.creator == C
+    assert y.creator.input == b
+    assert y.creator.input.creator == B
+    assert y.creator.input.creator.input == a
+    assert y.creator.input.creator.input.creator == A
+    assert y.creator.input.creator.input.creator.input == x
+
+
+def test_backward():
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    y.grad = np.array(1.0)
+    y.backward()
+    answer = 3.297442541400256
+    assert abs(answer - x.grad) < 1e-6
