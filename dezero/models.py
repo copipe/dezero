cd0373c816b09df35374419732b75f71c6f9db92
copipe <taka.0122.kuro@gmail.com>
2022-10-11 13:11:55 +0000

    add MLP class

--- dezero/models.py
+++ dezero/models.py
@@ -1,3 +1,7 @@
+from typing import List
+
+import dezero.functions as F
+import dezero.layers as L
 from dezero import Layer, Variable, utils
 
 
@@ -5,3 +9,24 @@ class Model(Layer):
     def plot(self, *inputs: Variable, to_file: str = "model.png"):
         y = self.forward(*inputs)
         return utils.plot_dot_graph(y, verbose=True, to_file=to_file)
+
+
+class MLP(Model):
+    def __init__(
+        self, input_size: int, fc_output_sizes: List[int], activation=F.sigmoid
+    ):
+        super().__init__()
+        self.activation = activation
+        self.layers = []
+
+        sizes = [input_size] + fc_output_sizes
+        for i in range(len(sizes) - 1):
+            in_size, out_size = sizes[i], sizes[i + 1]
+            layer = L.Linear(in_size, out_size)
+            setattr(self, "l" + str(i), layer)
+            self.layers.append(layer)
+
+    def forward(self, x: Variable) -> Variable:
+        for layer in self.layers[:-1]:
+            x = self.activation(layer(x))
+        return self.layers[-1](x)
--- steps/step45.py
+++ steps/step45.py
@@ -1,22 +1,8 @@
 import numpy as np
 
 import dezero.functions as F
-import dezero.layers as L
 from dezero import Variable
-from dezero.models import Model
-
-
-class TwoLayerNet(Model):
-    def __init__(self, in_size, hidden_size, out_size):
-        super().__init__()
-        self.l1 = L.Linear(in_size, hidden_size)
-        self.l2 = L.Linear(hidden_size, out_size)
-
-    def forward(self, x):
-        y = F.sigmoid(self.l1(x))
-        y = self.l2(y)
-        return y
-
+from dezero.models import MLP
 
 np.random.seed(0)
 x = np.random.rand(100, 1)
@@ -31,10 +17,10 @@ in_size = 1
 hidden_size = 10
 out_size = 1
 
-model = TwoLayerNet(in_size, hidden_size, out_size)
+model = MLP(in_size, [hidden_size, out_size])
 
 for i in range(iters):
-    y_pred = model(x)
+    y_pred = model(x)  # type:ignore
     loss = F.mean_squared_error(y, y_pred)
 
     model.cleargrads()
