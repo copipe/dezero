5c241dea552a26fa3f91ec086b8c3cf1af755abc
copipe <taka.0122.kuro@gmail.com>
2022-10-11 06:12:12 +0000

    add step43

---
+++ steps/step43.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+import dezero.functions as F
+from dezero import Variable
+
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
+x = Variable(x)  # type:ignore
+y = Variable(y)
+
+I, H, H2 = 1, 10, 1
+W1 = Variable(0.01 * np.random.randn(I, H))
+b1 = Variable(np.zeros(H))
+W2 = Variable(0.01 * np.random.randn(H, H2))
+b2 = Variable(np.zeros(H2))
+
+
+def predict(x):
+    y = F.linear(x, W1, b1)
+    y = F.sigmoid(y)
+    y = F.linear(y, W2, b2)
+    return y
+
+
+lr = 0.2
+iters = 10000
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    W1.cleargrad()
+    b1.cleargrad()
+    W2.cleargrad()
+    b2.cleargrad()
+    loss.backward()
+
+    assert W1.grad is not None
+    assert b1.grad is not None
+    assert W2.grad is not None
+    assert b2.grad is not None
+    W1.data -= lr * W1.grad.data
+    b1.data -= lr * b1.grad.data
+    W2.data -= lr * W2.grad.data
+    b2.data -= lr * b2.grad.data
+    if i % 1000 == 0:
+        print(loss)
