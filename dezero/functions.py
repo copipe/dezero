39d8bc0f38194101350d837f899f095f07b53d29
copipe <taka.0122.kuro@gmail.com>
2022-10-11 06:11:56 +0000

    add linear function & sigmoid function

--- dezero/functions.py
+++ dezero/functions.py
@@ -21,57 +21,6 @@ class Square(Function):
         return gx
 
 
-class Exp(Function):
-    """Forward and backward propagation of expornential operation."""
-
-    def forward(self, *xs: np.ndarray) -> np.ndarray:
-        return np.exp(xs[0])
-
-    def backward(self, gy: np.ndarray) -> np.ndarray:
-        x = self.inputs[0].data
-        gx = np.exp(x) * gy
-        return gx
-
-
-class Sin(Function):
-    """Forward and backward propagation of sin function."""
-
-    def forward(self, *xs: np.ndarray) -> np.ndarray:
-        y = np.sin(xs[0])
-        return as_array(y)
-
-    def backward(self, gy: Variable) -> Variable:
-        (x,) = self.inputs
-        gx = gy * cos(x)
-        return gx
-
-
-class Cos(Function):
-    """Forward and backward propagation of cos function."""
-
-    def forward(self, *xs: np.ndarray) -> np.ndarray:
-        y = np.cos(xs[0])
-        return as_array(y)
-
-    def backward(self, gy: Variable) -> Variable:
-        (x,) = self.inputs
-        gx = gy * -sin(x)
-        return gx
-
-
-class Tanh(Function):
-    """Forward and backward propagation of tanh function."""
-
-    def forward(self, *xs: np.ndarray) -> np.ndarray:
-        y = np.tanh(xs[0])
-        return as_array(y)
-
-    def backward(self, gy: Variable) -> Variable:
-        y = self.outputs[0]()  # outputs[0] is weakref
-        gx = gy * (1 - y**2)
-        return gx
-
-
 def square(x: Variable) -> Variable:
     """Perform a square operation.
 
@@ -84,6 +33,18 @@ def square(x: Variable) -> Variable:
     y = Square()(x)
     assert isinstance(y, Variable)
     return y
+
+
+class Exp(Function):
+    """Forward and backward propagation of expornential operation."""
+
+    def forward(self, *xs: np.ndarray) -> np.ndarray:
+        return np.exp(xs[0])
+
+    def backward(self, gy: np.ndarray) -> np.ndarray:
+        x = self.inputs[0].data
+        gx = np.exp(x) * gy
+        return gx
 
 
 def exp(x: Variable) -> Variable:
@@ -100,6 +61,19 @@ def exp(x: Variable) -> Variable:
     return y
 
 
+class Sin(Function):
+    """Forward and backward propagation of sin function."""
+
+    def forward(self, *xs: np.ndarray) -> np.ndarray:
+        y = np.sin(xs[0])
+        return as_array(y)
+
+    def backward(self, gy: Variable) -> Variable:
+        (x,) = self.inputs
+        gx = gy * cos(x)
+        return gx
+
+
 def sin(x: Variable) -> Variable:
     """Perform a sin function
 
@@ -109,7 +83,22 @@ def sin(x: Variable) -> Variable:
     Returns:
         Variable: output variable (sin(x))
     """
-    return Sin()(x)
+    y = Sin()(x)
+    assert isinstance(y, Variable)
+    return y
+
+
+class Cos(Function):
+    """Forward and backward propagation of cos function."""
+
+    def forward(self, *xs: np.ndarray) -> np.ndarray:
+        y = np.cos(xs[0])
+        return as_array(y)
+
+    def backward(self, gy: Variable) -> Variable:
+        (x,) = self.inputs
+        gx = gy * -sin(x)
+        return gx
 
 
 def cos(x: Variable) -> Variable:
@@ -121,7 +110,22 @@ def cos(x: Variable) -> Variable:
     Returns:
         Variable: output variable (cos(x))
     """
-    return Cos()(x)
+    y = Cos()(x)
+    assert isinstance(y, Variable)
+    return y
+
+
+class Tanh(Function):
+    """Forward and backward propagation of tanh function."""
+
+    def forward(self, *xs: np.ndarray) -> np.ndarray:
+        y = np.tanh(xs[0])
+        return as_array(y)
+
+    def backward(self, gy: Variable) -> Variable:
+        y = self.outputs[0]()  # outputs[0] is weakref
+        gx = gy * (1 - y**2)
+        return gx
 
 
 def tanh(x: Variable) -> Variable:
@@ -133,7 +137,10 @@ def tanh(x: Variable) -> Variable:
     Returns:
         Variable: output variable (tanh(x))
     """
-    return Tanh()(x)
+
+    y = Tanh()(x)
+    assert isinstance(y, Variable)
+    return y
 
 
 class Reshape(Function):
@@ -157,18 +164,6 @@ class Reshape(Function):
         return reshape(gy, self.x_shape)
 
 
-class Transpose(Function):
-    """Forward and backward propagation of transpose function."""
-
-    def forward(self, *xs: np.ndarray) -> np.ndarray:
-        y = np.transpose(xs[0])
-        return as_array(y)
-
-    def backward(self, gy: Variable) -> Variable:
-        gx = transpose(gy)
-        return gx
-
-
 def reshape(x: Variable, shape: Tuple) -> Variable:
     """Perform a reshape function
 
@@ -182,6 +177,18 @@ def reshape(x: Variable, shape: Tuple) -> Variable:
     if x.shape == shape:
         return as_variable(x)
     return Reshape(shape)(x)
+
+
+class Transpose(Function):
+    """Forward and backward propagation of transpose function."""
+
+    def forward(self, *xs: np.ndarray) -> np.ndarray:
+        y = np.transpose(xs[0])
+        return as_array(y)
+
+    def backward(self, gy: Variable) -> Variable:
+        gx = transpose(gy)
+        return gx
 
 
 def transpose(x: Variable) -> Variable:
@@ -262,6 +269,21 @@ class BroadcastTo(Function):
         return gx
 
 
+def broadcast_to(x: Variable, shape: Tuple[int, ...]):
+    """Perform a broadcast operation.
+
+    Args:
+        x (Variable): input variable
+        shape (Tuple): shape of the tensor after broadcasting
+
+    Returns:
+        Variable: output variable after broadcasting
+    """
+    if x.shape == shape:
+        return as_variable(x)
+    return BroadcastTo(shape)(x)
+
+
 class SumTo(Function):
     """Forward and backward propagation of sumto operation.
     This operation is opposite operation of broadcast.
@@ -283,6 +305,22 @@ class SumTo(Function):
     def backward(self, gy: Variable) -> Variable:
         gx = broadcast_to(gy, self.x_shape)
         return gx
+
+
+def sum_to(x: Variable, shape: Tuple[int, ...]):
+    """Perform a sumto operation.
+    This operation is opposite operation of broadcast.
+
+    Args:
+        x (Variable): input variable
+        shape (Tuple): shape of the tensor after sumto
+
+    Returns:
+        Variable: output variable after sumto
+    """
+    if x.shape == shape:
+        return as_variable(x)
+    return SumTo(shape)(x)
 
 
 class MatMul(Function):
@@ -313,32 +351,92 @@ def matmul(x0: Variable, x1: Variable) -> Variable:
     return MatMul()(x0, x1)
 
 
-def broadcast_to(x: Variable, shape: Tuple[int, ...]):
-    """Perform a broadcast operation.
+class MeanSquaredError(Function):
+    """Forward and backward propagation of MSE loss."""
+
+    def forward(self, *xs: np.ndarray) -> np.ndarray:
+        x0, x1 = xs
+        diff = x0 - x1
+        y = (diff**2).sum() / len(diff)
+        return as_array(y)
+
+    def backward(self, gy: Variable) -> Tuple[Variable, Variable]:
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        gy = broadcast_to(gy, diff.shape)
+        gx0 = gy * diff * (2.0 / len(diff))
+        gx1 = -gx0
+        return gx0, gx1
+
+
+def mean_squared_error(x0: Variable, x1: Variable) -> Variable:
+    """Compute MSE loss.
+
+    Args:
+        x0 (Variable): input variable (target or prediction)
+        x1 (Variable): input variable (prediction or target)
+
+    Returns:
+        Variable: output variable (sum{(x1 - x2)^2} / n)
+    """
+    return MeanSquaredError()(x0, x1)
+
+
+class Linear(Function):
+    """Forward and backward propagation of linear function."""
+
+    def forward(
+        self, x: np.ndarray, W: np.ndarray, b: np.ndarray | None = None
+    ) -> Variable:
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy: Variable) -> Tuple[Variable, Variable, Variable]:
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x: Variable, W: Variable, b: Variable | None = None) -> Variable:
+    """Applies a linear transformation to the incoming data (y = xW^T + b)
+    T
+     +b.
+
+        Args:
+            x (Variable): input Variable.
+            W (Variable): Weights.
+            b (Variable | None, optional): Bias. Defaults to None.
+
+        Returns:
+            Variable: output variable (xW^T + b)
+    """
+    return Linear()(x, W, b)
+
+
+class Sigmoid(Function):
+    """Forward and backward propagation of sigmoid function."""
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        y = 1 / (1 + np.exp(-x))
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x: Variable):
+    """Perform a sigmoid function
 
     Args:
         x (Variable): input variable
-        shape (Tuple): shape of the tensor after broadcasting
 
     Returns:
-        Variable: output variable after broadcasting
+        Variable: output variable (1/(1 + exp(-x)))
     """
-    if x.shape == shape:
-        return as_variable(x)
-    return BroadcastTo(shape)(x)
-
-
-def sum_to(x: Variable, shape: Tuple[int, ...]):
-    """Perform a sumto operation.
-    This operation is opposite operation of broadcast.
-
-    Args:
-        x (Variable): input variable
-        shape (Tuple): shape of the tensor after sumto
-
-    Returns:
-        Variable: output variable after sumto
-    """
-    if x.shape == shape:
-        return as_variable(x)
-    return SumTo(shape)(x)
+    return Sigmoid()(x)
